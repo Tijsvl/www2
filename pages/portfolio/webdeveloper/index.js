4b00594fc1dc8e8edf8f33122d6b3c72ebf445f3
Tijsvl <tijsvanleur@gmail.com>
2022-02-14 12:57:53 +0200

    Web Dev / Add animation-1 URL

--- pages/portfolio/webdeveloper/index.js
+++ pages/portfolio/webdeveloper/index.js
@@ -4,6 +4,7 @@ const WebDeveloperPage = () => {
   return (
     <section>
       <h1>Web Developer</h1>
+      <a href="/portfolio/webdeveloper/css/animation-1">Animation 1</a>
     </section>
   )
 }
