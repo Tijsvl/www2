aa170817448436b771def201dabead72d40e6a85
Tijsvl <tijsvanleur@gmail.com>
2023-08-14 23:04:30 +0200

    Image reference fix, Head updated

--- components/pages/QuickLinks/QuickLinks.jsx
+++ components/pages/QuickLinks/QuickLinks.jsx
@@ -12,12 +12,25 @@ const QuickLinks = () => {
 
   return (
     <>
+      <Head>
+        <title>@tijsvl | Privacy</title>
+        <meta property="og:title" content="Quick Links" />
+        <meta name="twitter:title" content="Quick Links" />
+        <meta property="og:description" content="Quick Links to current content on tijsvl.net" />
+        <meta name="twitter:description" content="Quick Links to current content on tijsvl.net" />
+        <meta name="description" content="Quick Links to current content on tijsvl.net" />
+        <meta property="og:image" content="https://tijsvl.net/img/og_default.jpg" />
+        <meta name="twitter:image" content="https://tijsvl.net/img/og_default.jpg" />
+        <link rel="canonical" href="https://tijsvl.net/quick-links" />
+        <meta property="og:url" content="https://tijsvl.net/quick-links" />
+        <meta name="twitter:url" content="https://tijsvl.net/quick-links" />
+      </Head>
       <h1 className={classes.title}>Quick Links</h1>
       <ul className={classes.quicklinks}>
         <li className={classes.gallery}>
           <a href="/galleries/drain-20230812">
             <div className={classes.image}>
-              <img src="https://tijsvl.net/static/gallery/drain-20230812/default-low.jpg" alt="Live Photo" />
+              <img src="https://tijsvl.net/static/galleries/drain-20230812/default-low.jpg" alt="Live Photo" />
             </div>
             <div className={classes.info}>
               <span className={classes.artist}>Drain</span>
