6638c926eb2f0cd9a66f16799753ae543a9f8169
Tijsvl <tijsvanleur@gmail.com>
2023-08-16 21:18:25 +0200

    Added STYG

--- components/pages/QuickLinks/QuickLinks.jsx
+++ components/pages/QuickLinks/QuickLinks.jsx
@@ -15,19 +15,6 @@ const QuickLinks = () => {
 
   return (
     <>
-      <Head>
-        <title>@tijsvl | Quick Links</title>
-        <meta property="og:title" content="Quick Links" />
-        <meta name="twitter:title" content="Quick Links" />
-        <meta property="og:description" content="Quick Links to current content on tijsvl.net" />
-        <meta name="twitter:description" content="Quick Links to current content on tijsvl.net" />
-        <meta name="description" content="Quick Links to current content on tijsvl.net" />
-        <meta property="og:image" content="https://tijsvl.net/img/og_default.jpg" />
-        <meta name="twitter:image" content="https://tijsvl.net/img/og_default.jpg" />
-        <link rel="canonical" href="https://tijsvl.net/quick-links" />
-        <meta property="og:url" content="https://tijsvl.net/quick-links" />
-        <meta name="twitter:url" content="https://tijsvl.net/quick-links" />
-      </Head>
       <h1 className={classes.title}>Quick Links</h1>
       <ul className={classes.quicklinks}>
         <li className={classes.gallery}>
@@ -38,6 +25,21 @@ const QuickLinks = () => {
               </div>
               <div className={classes.info}>
                 <span className={classes.artist}>Drain</span>
+                <span className={classes.detail}>Tells Bells Festival</span>
+                <span className={classes.detail}>August 14, 2023</span>
+              </div>
+              <span className={classes.tag}>Gallery</span>
+            </a>
+          </Link>
+        </li>
+        <li className={classes.gallery}>
+          <Link href="/gallery/stick-to-your-guns-20230812">
+            <a>
+              <div className={classes.image}>
+                <img src="https://tijsvl.net/static/galleries/stick-to-your-guns-20230812/default-low.jpg" alt="Live Photo" />
+              </div>
+              <div className={classes.info}>
+                <span className={classes.artist}>Stick To Your Guns</span>
                 <span className={classes.detail}>Tells Bells Festival</span>
                 <span className={classes.detail}>August 14, 2023</span>
               </div>
--- pages/quick-links/index.jsx
+++ pages/quick-links/index.jsx
@@ -6,15 +6,17 @@ const QuickLinksPage = () => {
   return (
     <>
       <Head>
-        <title>@tijsvl | QuickLinks</title>
-        <meta property="og:title" content="@tijsvl | QuickLinks" />
-        <meta name="twitter:title" content="@tijsvl | QuickLinks" />
-        <meta property="og:description" content="Quick Links to current content" />
-        <meta name="twitter:description" content="Quick Links to current content" />
-        <meta name="description" content="Quick Links to current content" />
-        <link rel="canonical" href="https://tijsvl.net/quicklinks" />
-        <meta property="og:url" content="https://tijsvl.net/quicklinks" />
-        <meta name="twitter:url" content="https://tijsvl.net/quicklinks" />
+        <title>@tijsvl | Quick Links</title>
+        <meta property="og:title" content="Quick Links" />
+        <meta name="twitter:title" content="Quick Links" />
+        <meta property="og:description" content="Quick Links to current content on tijsvl.net" />
+        <meta name="twitter:description" content="Quick Links to current content on tijsvl.net" />
+        <meta name="description" content="Quick Links to current content on tijsvl.net" />
+        <meta property="og:image" content="https://tijsvl.net/img/og_default.jpg" />
+        <meta name="twitter:image" content="https://tijsvl.net/img/og_default.jpg" />
+        <link rel="canonical" href="https://tijsvl.net/quick-links" />
+        <meta property="og:url" content="https://tijsvl.net/quick-links" />
+        <meta name="twitter:url" content="https://tijsvl.net/quick-links" />
       </Head>
       <QuickLinks />
     </>
