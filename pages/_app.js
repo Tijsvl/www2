46e60e21a057adef25f74b3414dd84b9fb574521
Tijsvl <tijsvanleur@gmail.com>
2023-10-17 22:42:57 +0200

    Added ads

--- pages/_app.js
+++ pages/_app.js
@@ -10,51 +10,49 @@ function MyApp({ Component, pageProps }) {
   return (
     <AuthContextProvider>
       <Head>
-        <meta charSet='utf-8' />
-        <meta name='robots' content='index, follow' />
-        <meta content='width=device-width, initial-scale=1.0' name='viewport' />
-        <meta name='HandheldFriendly' content='True' />
-        <meta name='format-detection' content='telephone=yes' />
-        <meta name='mobile-web-app-capable' content='yes' />
-        <meta name='apple-mobile-web-app-capable' content='yes' />
-        <link rel='shortcut icon' href='/img/style/fav.ico' />
-        <link rel='apple-touch-icon' href='/img/style/fav-apple.png' sizes='180x180' />
-        <link rel='icon' href='/img/style/fav.svg' sizes='16x16' type='image/svg+xml' />
-        <link rel='icon' href='/img/style/fav.ico' type='image/vnd.microsoft.icon' />
-        <link rel='icon' href='/img/style/fav-512.png' sizes='512x512' type='image/png' />
-        <link rel='icon' href='/img/style/fav-256.png' sizes='256x256' type='image/png' />
-        <link rel='icon' href='/img/style/fav-128.png' sizes='128x128' type='image/png' />
-        <link rel='icon' href='/img/style/fav-64.png' sizes='64x64' type='image/png' />
-        <link rel='icon' href='/img/style/fav-48.png' sizes='48x48' type='image/png' />
-        <link rel='icon' href='/img/style/fav-32.png' sizes='32x32' type='image/png' />
-        <link rel='icon' href='/img/style/fav-16.png' sizes='16x16' type='image/png' />
-        <link rel='icon' href='/img/style/fav-16.png' sizes='16x16' type='image/png' />
-        <link rel='mask-icon' href='/img/style/fav.svg' color='#FDFDFD' />
-        <link rel='manifest' href='/manifest.webmanifest' />
-        <meta name='theme-color' content='#010101' />
-        <meta name='msapplication-TileColor' content='#010101' />
-        <meta name='msapplication-TileImage' content='/img/style/fav-150.png' />
+        <meta charSet="utf-8" />
+        <meta name="robots" content="index, follow" />
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+        <meta name="HandheldFriendly" content="True" />
+        <meta name="format-detection" content="telephone=yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <link rel="shortcut icon" href="/img/style/fav.ico" />
+        <link rel="apple-touch-icon" href="/img/style/fav-apple.png" sizes="180x180" />
+        <link rel="icon" href="/img/style/fav.svg" sizes="16x16" type="image/svg+xml" />
+        <link rel="icon" href="/img/style/fav.ico" type="image/vnd.microsoft.icon" />
+        <link rel="icon" href="/img/style/fav-512.png" sizes="512x512" type="image/png" />
+        <link rel="icon" href="/img/style/fav-256.png" sizes="256x256" type="image/png" />
+        <link rel="icon" href="/img/style/fav-128.png" sizes="128x128" type="image/png" />
+        <link rel="icon" href="/img/style/fav-64.png" sizes="64x64" type="image/png" />
+        <link rel="icon" href="/img/style/fav-48.png" sizes="48x48" type="image/png" />
+        <link rel="icon" href="/img/style/fav-32.png" sizes="32x32" type="image/png" />
+        <link rel="icon" href="/img/style/fav-16.png" sizes="16x16" type="image/png" />
+        <link rel="icon" href="/img/style/fav-16.png" sizes="16x16" type="image/png" />
+        <link rel="mask-icon" href="/img/style/fav.svg" color="#FDFDFD" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <meta name="theme-color" content="#010101" />
+        <meta name="msapplication-TileColor" content="#010101" />
+        <meta name="msapplication-TileImage" content="/img/style/fav-150.png" />
         <title>@tijsvl | Event Photography &amp; Web Developer</title>
-        <meta name='author' content='Tijs van Leur' />
-        <meta property='og:locale' content='en_US' />
-        <meta property='og:site_name' content='tijsvl.com' />
-        <meta property='og:title' content='@tijsvl | Event Photography &amp; Web Developer' />
-        <meta property='og:description' content="On tour with your favorite band or at home coding what you'll never see." />
-        <meta property='og:image' content='https://www.tijsvl.com/media/og/default.jpg' />
-        <meta property='og:image:alt' content='https://www.tijsvl.com/media/og/default2.jpg' />
-        <meta property='og:image:alt' content='https://www.tijsvl.com/img/p/w/iceland_2019.09.27_17.49.46__K7A8552-2000.jpg' />
-        <meta property='og:url' content='https://www.tijsvl.com/' />
-        <meta property='og:type' content='page' />
-        <meta name='twitter:card' content='summary' />
-        <meta name='twitter:site' content='tijsvl.com' />
-        <meta name='twitter:title' content='@tijsvl | Event Photography &amp; Web Developer' />
-        <meta name='twitter:description' content="On tour with your favorite band or at home coding what you'll never see." />
-        <meta name='twitter:image' content='https://www.tijsvl.com/media/og/default.jpg' />
-        <meta name='twitter:url' content='https://www.tijsvl.com/' />
-        <meta name='description' content="On tour with your favorite band or at home coding what you'll never see." />
-        <link rel='canonical' href='https://www.tijsvl.com/' />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6060589550732180"
-     crossorigin="anonymous"></script>
+        <meta name="author" content="Tijs van Leur" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="tijsvl.com" />
+        <meta property="og:title" content="@tijsvl | Event Photography &amp; Web Developer" />
+        <meta property="og:description" content="On tour with your favorite band or at home coding what you'll never see." />
+        <meta property="og:image" content="https://www.tijsvl.com/media/og/default.jpg" />
+        <meta property="og:image:alt" content="https://www.tijsvl.com/media/og/default2.jpg" />
+        <meta property="og:image:alt" content="https://www.tijsvl.com/img/p/w/iceland_2019.09.27_17.49.46__K7A8552-2000.jpg" />
+        <meta property="og:url" content="https://www.tijsvl.com/" />
+        <meta property="og:type" content="page" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="tijsvl.com" />
+        <meta name="twitter:title" content="@tijsvl | Event Photography &amp; Web Developer" />
+        <meta name="twitter:description" content="On tour with your favorite band or at home coding what you'll never see." />
+        <meta name="twitter:image" content="https://www.tijsvl.com/media/og/default.jpg" />
+        <meta name="twitter:url" content="https://www.tijsvl.com/" />
+        <meta name="description" content="On tour with your favorite band or at home coding what you'll never see." />
+        <link rel="canonical" href="https://www.tijsvl.com/" />
       </Head>
       <Layout>
         <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -10,26 +10,27 @@ const Index = () => {
     <>
       <Head>
         <title>@tijsvl | Event Photography and Web Developer</title>
-        <meta property='og:title' content='@tijsvl | Event Photography and Web Developer' />
-        <meta name='twitter:title' content='@tijsvl | Event Photography and Web Developer' />
-        <meta property='og:description' content="On tour with your favorite band or at home coding what you'll never see." />
-        <meta name='twitter:description' content="On tour with your favorite band or at home coding what you'll never see." />
-        <meta name='description' content="On tour with your favorite band or at home coding what you'll never see." />
-        <meta property='og:image' content='/img/p/m/macklemore__august-17-2019-macklemore-st-polten-austria___K7A9724-2000.jpg' />
-        <meta property='og:image:alt' content='/img/p/m/sum-41__january-22-2020-sum-41-amsterdam-the-netherlands__3R7A7252-2000.jpg' />
-        <meta property='og:image:alt' content='/img/p/m/offspring__july-9-2018-the-offspring-fete-du-bruit-landerneau-france__3R7A9815-2000.jpg' />
-        <meta property='og:image:alt' content='/img/p/m/bullet-for-my-valentine__august-11-2019-bullet-for-my-valentine-eschwege-germany___K7A3790-2000.jpg' />
-        <meta name='twitter:image' content='/img/p/m/evergreen-terrace__august-10-2018-evergreen-terrace-villmar-germany__3R7A2713-2000.jpg' />
-        <link rel='canonical' href='https://www.tijsvl.com/portfolio/music' />
-        <meta property='og:url' content='https://www.tijsvl.com/portfolio/music' />
-        <meta name='twitter:url' content='https://www.tijsvl.com/portfolio/music' />
-        <meta name='facebook-domain-verification' content='2wrx1fif0w7nezmgf7s9cm4qsfdzhm' />
+        <meta property="og:title" content="@tijsvl | Event Photography and Web Developer" />
+        <meta name="twitter:title" content="@tijsvl | Event Photography and Web Developer" />
+        <meta property="og:description" content="On tour with your favorite band or at home coding what you'll never see." />
+        <meta name="twitter:description" content="On tour with your favorite band or at home coding what you'll never see." />
+        <meta name="description" content="On tour with your favorite band or at home coding what you'll never see." />
+        <meta property="og:image" content="/img/p/m/macklemore__august-17-2019-macklemore-st-polten-austria___K7A9724-2000.jpg" />
+        <meta property="og:image:alt" content="/img/p/m/sum-41__january-22-2020-sum-41-amsterdam-the-netherlands__3R7A7252-2000.jpg" />
+        <meta property="og:image:alt" content="/img/p/m/offspring__july-9-2018-the-offspring-fete-du-bruit-landerneau-france__3R7A9815-2000.jpg" />
+        <meta property="og:image:alt" content="/img/p/m/bullet-for-my-valentine__august-11-2019-bullet-for-my-valentine-eschwege-germany___K7A3790-2000.jpg" />
+        <meta name="twitter:image" content="/img/p/m/evergreen-terrace__august-10-2018-evergreen-terrace-villmar-germany__3R7A2713-2000.jpg" />
+        <link rel="canonical" href="https://www.tijsvl.com/portfolio/music" />
+        <meta property="og:url" content="https://www.tijsvl.com/portfolio/music" />
+        <meta name="twitter:url" content="https://www.tijsvl.com/portfolio/music" />
+        <meta name="facebook-domain-verification" content="2wrx1fif0w7nezmgf7s9cm4qsfdzhm" />
         <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
         <script
           dangerouslySetInnerHTML={{
             __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}',{page_path: window.location.pathname,});`
           }}
         />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6060589550732180" crossorigin="anonymous"></script>
       </Head>
       <MusicPage />
     </>
